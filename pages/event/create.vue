f688e33cb35a57f031e6221e3cdb8fdf883b8516
Yedukondala Reddy <yedukondala.reddy@profiledevelopments.com>
2020-05-05 08:14:16 +0100

    Adding Event Service and listing events

---
+++ pages/event/create.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Create Event</h1>
+  </div>
+</template>
+<script>
+export default {
+  head() {
+    return {
+      title: 'Create an Event',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'participate to create a real world events'
+        }
+      ]
+    }
+  }
+}
+</script>
